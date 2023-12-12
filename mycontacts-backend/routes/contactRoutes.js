8ad461400cae4d12dfce47db7cdcfbc93a5ac961
raoasif <raomuhammadasif91@gmail.com>
2023-12-12 12:25:32 +0500

    added the user controller and routes.

--- mycontacts-backend/routes/contactRoutes.js
+++ mycontacts-backend/routes/contactRoutes.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const {    
-    getContacts, 
-    getContact, 
-    createContact, 
-    updateContact, 
-    deleteContact
+const {
+  getContacts,
+  getContact,
+  createContact,
+  updateContact,
+  deleteContact,
 } = require("../controllers/contactController");
 
 router.route("/").get(getContacts).post(createContact);
